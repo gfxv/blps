3450e61b328ddfa76d86cc50a2599023cd1b6599
Maria <ivanovaaaaaa45@gmail.com>
2025-05-08 16:34:03 +0300

    feat: scheduled for comments and monetization

--- src/main/java/dev/gfxv/blps/BlpsApplication.java
+++ src/main/java/dev/gfxv/blps/BlpsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BlpsApplication {
 
--- src/main/java/dev/gfxv/blps/repository/UserRepository.java
+++ src/main/java/dev/gfxv/blps/repository/UserRepository.java
@@ -3,6 +3,7 @@ package dev.gfxv.blps.repository;
 import dev.gfxv.blps.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
+    List<User> findByMonetizedTrue();
 }
